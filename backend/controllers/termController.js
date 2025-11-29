0059769ee8c1ac0aa0ec4da0247514a054873e61
hieu <hieuokchua@gmail.com>
2025-11-29 12:05:18 +0700

    Viết lại chức năng sao chép tiêu chí và chỉnh cách hiển thị của chức năng tì kiếm 	modified:   backend/controllers/adminController.js 	modified:   backend/controllers/termController.js 	modified:   frontend/src/components/admin/StudentSearchDetails.jsx 	modified:   frontend/src/pages/admin/SearchStudentsPage.jsx

--- backend/controllers/termController.js
+++ backend/controllers/termController.js
@@ -8,7 +8,7 @@ export const getAllTerms = async (req, res, next) => {
       `SELECT code, title, year, semester, is_active
        FROM ref.term
        ORDER BY year DESC, semester DESC
-       Limit 2;
+       Limit 3;
        `
     );
     res.json(rows);
