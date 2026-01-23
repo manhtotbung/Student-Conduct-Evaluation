f1cf2fa0127e3011193f4d07af815fae93c8a280
ManhDo <dophuongmanh.khlmr@gmail.com>
2026-01-23 13:42:24 +0700

    change db url

--- backend/db.js
+++ backend/db.js
@@ -11,11 +11,7 @@ dotenv.config({ path: path.join(__dirname, '..', '.env') });
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.PGSSL === 'true' ? { rejectUnauthorized: false } : false,
   idleTimeoutMillis: 30000,
 });
